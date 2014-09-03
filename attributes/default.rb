d87a965e034943f566b2bee7e1e8a73271b4c33b
Nick Muerdter <nick.muerdter@nrel.gov>
2014-09-03 09:02:47 -0600

    Download to the chef cache dir by default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default["hdf5"]["version"] = "1.8.13"
 default["hdf5"]["download_url"] = "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-#{node["hdf5"]["version"]}/src/hdf5-#{node["hdf5"]["version"]}.tar.bz2"
-default["hdf5"]["download_dir"] = "/tmp"
+default["hdf5"]["download_dir"] = Chef::Config[:file_cache_path]
 default["hdf5"]["checksum"] = "c2f5a412107aba6f99fd7a4a9db6ce5f5fc8171ec931472784e5839d26aa17ef"
 default["hdf5"]["prefix"] = "/usr/local"
